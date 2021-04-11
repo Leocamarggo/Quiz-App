ccc39e70e7a1a23b6db2a7b3ad67977801abb3ba
Leocamarggo <leonardo.camarggo@outlook.com>
2021-04-11 02:52:22 -0300

    Update with questions and roles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import React, { useState } from 'react';
+import {questoes} from './components/questoes.js';
 
-function App() {
-  return (
-    <div className="App">
+export default function App() {
 
-    </div>
-  );
+	const [questaoAtual, setQuestaoAtual] = useState(0);
+	const [showPont, setShowPont] = useState(false);
+	const [pont, setPont] = useState(0);
+
+	const handleClick = (correto) => {
+		if (correto) {
+			setPont(pont + 1);
+		}
+
+		const proxQuestao = questaoAtual + 1;
+		if (proxQuestao < questoes.length) {
+			setQuestaoAtual(proxQuestao);
+		} else {
+			setShowPont(true);
+		}
+	};
+
+	return (
+		<div className='app'>
+			{showPont ? (
+				<div>
+					Sua pontuação foi de {pont} de {questoes.length}
+				</div>
+			) : (
+				<>
+					<div>
+						<div>
+							<span>Questão {questaoAtual + 1}</span>/{questoes.length}
+						</div>
+						<div>{questoes[questaoAtual].questao}</div>
+					</div>
+					<div>
+						{questoes[questaoAtual].opcoesRespostas.map((opcaoResposta) => (
+							<button onClick={() => handleClick(opcaoResposta.correto)}>{opcaoResposta.respostas}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
---
+++ src/components/questoes.js
@@ -0,0 +1,47 @@
+export const questoes = [
+    {
+        questao: 'Quem fundou a amazon?',
+        opcoesRespostas: [
+            { respostas: 'Elon Musk', correto: false },
+            { respostas: 'Mark Zuckerberg', correto: false },
+            { respostas: 'Jeff Bezos', correto: true },
+            { respostas: 'Bill Gates', correto: false },
+        ],
+    },
+    {
+        questao: 'Qual a capital do Brasil?',
+        opcoesRespostas: [
+            { respostas: 'Belo Horizonte', correto: false },
+            { respostas: 'Brasília', correto: true },
+            { respostas: 'Curitiba', correto: false },
+            { respostas: 'São Paulo', correto: false },
+        ],
+    },
+    {
+        questao: 'A plataforma "Youtube" pertence a qual empresa?',
+        opcoesRespostas: [
+            { respostas: 'Google', correto: true },
+            { respostas: 'Facebook', correto: false },
+            { respostas: 'Amazon', correto: false },
+            { respostas: 'Microsoft', correto: false },
+        ],
+    },
+    {
+        questao: 'Quanto é 13 dividido por 2?',
+        opcoesRespostas: [
+            { respostas: '7', correto: false },
+            { respostas: '7.5', correto: false },
+            { respostas: '6', correto: false },
+            { respostas: '6.5', correto: true },
+        ],
+    },
+    {
+        questao: 'Por quanto o app "Instagram" foi comprado?',
+        opcoesRespostas: [
+            { respostas: 'US$ 1,3 Bilhões', correto: false },
+            { respostas: 'US$ 2 Bilhões', correto: false },
+            { respostas: 'US$ 1 Bilhão', correto: true },
+            { respostas: 'US$ 3 Bilhões', correto: false },
+        ],
+    }
+];
