b02c25af88f9196c1230039c7ff30f629137bc5d
Leocamarggo <leonardo.camarggo@outlook.com>
2021-04-12 21:53:48 -0300

    Initial customization update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React, { useState } from 'react';
+import {
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	Typography,
+	Button,
+	DialogActions,
+	Link,
+	List,
+	ListItem
+} from '@material-ui/core';
 import {questoes} from './components/questoes.js';
 
 export default function App() {
@@ -20,8 +31,35 @@ export default function App() {
 		}
 	};
 
+	const [open, setOpen] = useState(false);
+	const closeDialog = () => {
+		setOpen(false);
+	};
+
 	return (
 		<div className='app'>
+			<Dialog aria-labelledby="customized-dialog-title" open={open}>
+				<DialogTitle id="customized-dialog-title">
+					Bem vindo ao Quizz
+				</DialogTitle>
+				<DialogContent dividers>
+					<Typography gutterBottom>
+						Esta aplicação foi desenvolvida com React e MaterialUI.
+					</Typography>
+					<Typography gutterBottom>
+						Neste questionario você encontrará perguntas simples sobre tecnologia em geral.
+					</Typography>
+					<Typography gutterBottom>
+						By: <Link href="https://github.com/Leocamarggo">Leonaro Camargo</Link> 
+					</Typography>
+				</DialogContent>
+				<DialogActions>
+					<Button color="primary" onClick={closeDialog}>
+						Vamos nessa!
+					</Button>
+				</DialogActions>
+			</Dialog>
+
 			{showPont ? (
 				<div>
 					Sua pontuação foi de {pont} de {questoes.length}
@@ -30,14 +68,18 @@ export default function App() {
 				<>
 					<div>
 						<div>
-							<span>Questão {questaoAtual + 1}</span>/{questoes.length}
+							<span>Questão {questaoAtual + 1}</span> / {questoes.length}
 						</div>
 						<div>{questoes[questaoAtual].questao}</div>
 					</div>
-					<div>
-						{questoes[questaoAtual].opcoesRespostas.map((opcaoResposta) => (
-							<button onClick={() => handleClick(opcaoResposta.correto)}>{opcaoResposta.respostas}</button>
-						))}
+					<div style={{height: 300, width: 500}}>
+						<List>
+							{questoes[questaoAtual].opcoesRespostas.map((opcaoResposta) => (
+								<ListItem button onClick={() => handleClick(opcaoResposta.correto)}>
+									{opcaoResposta.respostas}
+								</ListItem>
+							))}
+						</List>
 					</div>
 				</>
 			)}
