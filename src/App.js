8d3ed267ac2e52bc3f62df21beb9091b3918db87
Leocamarggo <leonardo.camarggo@outlook.com>
2022-01-29 01:33:14 -0300

    FInal updates

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 	ListItem
 } from '@material-ui/core';
 import {questoes} from './components/questoes.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
 
@@ -31,11 +32,50 @@ export default function App() {
 		}
 	};
 
-	const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(true);
 	const closeDialog = () => {
 		setOpen(false);
 	};
 
+	const ResultQuestions = (point, question) =>{
+		if(point >= 3){
+			return(
+				<>
+					<Dialog aria-labelledby="customized-dialog-title" open={true}>
+						<DialogTitle id="customized-dialog-title">
+							Parabénss 🎉🎉
+						</DialogTitle>
+						<DialogContent dividers>
+							<Typography gutterBottom>
+								Sua pontuação foi de {point} de {question.length}
+							</Typography>
+						</DialogContent>
+					</Dialog>
+				</>
+			)
+		}else{
+			return(
+				<>
+					<Dialog aria-labelledby="customized-dialog-title" open={true}>
+						<DialogTitle id="customized-dialog-title">
+							Da pra melhorar em!? 😱
+						</DialogTitle>
+						<DialogContent dividers>
+							<Typography>
+								Sua pontuação foi de {point} de {question.length}
+							</Typography>
+						</DialogContent>
+						<DialogActions>
+							<Button className='bg-success text-white py-3' fullWidth onClick={()=>{window.location.reload()}}>
+								Tentar novamente
+							</Button>
+						</DialogActions>
+					</Dialog>
+				</>
+			)
+		}
+
+	}
 	return (
 		<div className='app'>
 			<Dialog aria-labelledby="customized-dialog-title" open={open}>
@@ -44,17 +84,18 @@ export default function App() {
 				</DialogTitle>
 				<DialogContent dividers>
 					<Typography gutterBottom>
-						Esta aplicação foi desenvolvida com React e MaterialUI.
+						Esta simples aplicação foi desenvolvida utilizando: React, MaterialUI e React-Bootstrap.
 					</Typography>
 					<Typography gutterBottom>
-						Neste questionario você encontrará perguntas simples sobre tecnologia em geral.
+						Neste breve questionario você encontrará algumas perguntas sobre o mundo de tecnologia.
 					</Typography>
-					<Typography gutterBottom>
-						By: <Link href="https://github.com/Leocamarggo">Leonaro Camargo</Link> 
-					</Typography>
+
 				</DialogContent>
-				<DialogActions>
-					<Button color="primary" onClick={closeDialog}>
+				<DialogActions style={{padding: 16, justifyContent: 'space-between'}}>
+					<Typography gutterBottom>
+						By: <Link href="https://github.com/Leocamarggo">Leonardo Camargo</Link> 
+					</Typography>
+					<Button className='bg-success text-white py-3 px-5' onClick={closeDialog}>
 						Vamos nessa!
 					</Button>
 				</DialogActions>
@@ -62,24 +103,34 @@ export default function App() {
 
 			{showPont ? (
 				<div>
-					Sua pontuação foi de {pont} de {questoes.length}
+					{ResultQuestions(pont, questoes)}
 				</div>
 			) : (
 				<>
-					<div>
-						<div>
-							<span>Questão {questaoAtual + 1}</span> / {questoes.length}
+					<div className='container d-flex justify-content-center align-items-center vh-100'>
+						<div style={{width: 400}} class="card text-center border-success">
+							<div class="card-header bg-success p-3">
+								<h5 className='text-white m-0'>
+									<span>Questão {questaoAtual + 1}</span> / {questoes.length}
+								</h5>
+							</div>
+							<div class="card-body">
+								<div className='mt-1'>
+									<h5 class="card-title">
+										{questoes[questaoAtual].questao}
+									</h5>
+								</div>
+								<div className='mt-3' style={{height: 200}}>
+									<List className='card-text bg-success text-white rounded'>
+										{questoes[questaoAtual].opcoesRespostas.map((opcaoResposta) => (
+											<ListItem button onClick={() => handleClick(opcaoResposta.correto)}>
+												<h6>{opcaoResposta.respostas}</h6>
+											</ListItem>
+										))}
+									</List>
+								</div>
+							</div>
 						</div>
-						<div>{questoes[questaoAtual].questao}</div>
-					</div>
-					<div style={{height: 300, width: 500}}>
-						<List>
-							{questoes[questaoAtual].opcoesRespostas.map((opcaoResposta) => (
-								<ListItem button onClick={() => handleClick(opcaoResposta.correto)}>
-									{opcaoResposta.respostas}
-								</ListItem>
-							))}
-						</List>
 					</div>
 				</>
 			)}
--- src/components/questoes.js
+++ src/components/questoes.js
@@ -2,46 +2,46 @@ export const questoes = [
     {
         questao: 'Quem fundou a amazon?',
         opcoesRespostas: [
-            { respostas: 'Elon Musk', correto: false },
-            { respostas: 'Mark Zuckerberg', correto: false },
-            { respostas: 'Jeff Bezos', correto: true },
-            { respostas: 'Bill Gates', correto: false },
+            { respostas: '1 - Elon Musk', correto: false },
+            { respostas: '2 - Mark Zuckerberg', correto: false },
+            { respostas: '3 - Jeff Bezos', correto: true },
+            { respostas: '4 - Bill Gates', correto: false },
         ],
     },
     {
         questao: 'Qual a capital do Brasil?',
         opcoesRespostas: [
-            { respostas: 'Belo Horizonte', correto: false },
-            { respostas: 'Brasília', correto: true },
-            { respostas: 'Curitiba', correto: false },
-            { respostas: 'São Paulo', correto: false },
+            { respostas: '1 - Belo Horizonte', correto: false },
+            { respostas: '2 - Brasília', correto: true },
+            { respostas: '3 - Curitiba', correto: false },
+            { respostas: '4 - São Paulo', correto: false },
         ],
     },
     {
         questao: 'A plataforma "Youtube" pertence a qual empresa?',
         opcoesRespostas: [
-            { respostas: 'Google', correto: true },
-            { respostas: 'Facebook', correto: false },
-            { respostas: 'Amazon', correto: false },
-            { respostas: 'Microsoft', correto: false },
+            { respostas: '1 - Google', correto: true },
+            { respostas: '2 - Facebook', correto: false },
+            { respostas: '3 - Amazon', correto: false },
+            { respostas: '4 - Microsoft', correto: false },
         ],
     },
     {
         questao: 'Quanto é 13 dividido por 2?',
         opcoesRespostas: [
-            { respostas: '7', correto: false },
-            { respostas: '7.5', correto: false },
-            { respostas: '6', correto: false },
-            { respostas: '6.5', correto: true },
+            { respostas: '1 - 7', correto: false },
+            { respostas: '2 - 7.5', correto: false },
+            { respostas: '3 - 6', correto: false },
+            { respostas: '4 - 6.5', correto: true },
         ],
     },
     {
         questao: 'Por quanto o app "Instagram" foi comprado?',
         opcoesRespostas: [
-            { respostas: 'US$ 1,3 Bilhões', correto: false },
-            { respostas: 'US$ 2 Bilhões', correto: false },
-            { respostas: 'US$ 1 Bilhão', correto: true },
-            { respostas: 'US$ 3 Bilhões', correto: false },
+            { respostas: '1 - US$ 1,3 Bilhões', correto: false },
+            { respostas: '2 - US$ 2 Bilhões', correto: false },
+            { respostas: '3 - US$ 1 Bilhão', correto: true },
+            { respostas: '4 - US$ 3 Bilhões', correto: false },
         ],
     }
 ];
